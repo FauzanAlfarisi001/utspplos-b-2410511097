1beeeaf815cba49c73104d0810acf20b34687175
FauzanAlfarisi001 <fauzanasp4541@gmail.com>
2026-04-28 19:20:38 +0700

    feat(complaint-php): CodeIgniter 4 installation for complaint service with code for API routes

---
+++ services/complaint-service/app/Config/Routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->group('api', function($routes) {
+    $routes->get('complaints', 'ComplaintController::index');
+    $routes->get('complaints/(:num)', 'ComplaintController::show/$1');
+    $routes->post('complaints', 'ComplaintController::create');
+    $routes->put('complaints/(:num)', 'ComplaintController::update/$1');
+    $routes->delete('complaints/(:num)', 'ComplaintController::delete/$1');
+    $routes->get('complaints/(:num)/responses','ComplaintController::responses/$1');
+    $routes->post('complaints/(:num)/responses','ComplaintController::addResponse/$1');
+
+    $routes->get('categories', 'CategoryController::index');
+    $routes->post('categories', 'CategoryController::create');
+
+    // internal
+    $routes->get('complaints/internal/(:num)', 'ComplaintController::internalShow/$1');
+    $routes->put('complaints/(:num)/status', 'ComplaintController::updateStatus/$1');
+});
